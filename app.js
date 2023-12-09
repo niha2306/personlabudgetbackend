358a9ec591c2acc6af531452f32719663f56d008
sai Niharika <sainikki2306@gmail.com>
2023-12-09 16:09:12 +0530

    updated cors

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const app = express();
 app.use(cors({
     origin: true
 }));
+
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
